7abd4f84e8fad1f37f1aec59ff8845514fd9d389
Nguyen Thien Thien <kinaad24@gmail.com>
2024-07-17 11:19:19 +0700

    #9 base DetailsOrder

---
+++ fastfood_application/app/src/main/java/com/example/fastfood/activities/DetailOrder.kt
@@ -0,0 +1,9 @@
+package com.example.fastfood.activities
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun DetailsOrder(navController: NavController){
+
+}
